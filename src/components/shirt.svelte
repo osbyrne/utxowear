ff833ed3d60341b7ba08adb4748f9f06d7518c55
osbyrne <osbyrne@skiff.com>
2024-11-17 11:54:06 +0100

    added svelte. broke svelte. fix next time pls

---
+++ src/components/shirt.svelte
@@ -0,0 +1,16 @@
+<script>
+  let color = 'blue';
+
+  function handleClick() {
+    color = color === 'blue' ? 'red' : 'blue';
+  }
+</script>
+
+<button
+  style={`background-color: ${color}; color: white; padding: 10px 20px; border-radius: 5px; cursor: pointer`}
+  on:click={handleClick}
+>
+  Change Color
+</button>
+
+export default button
